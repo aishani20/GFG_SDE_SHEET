c035dcd151fe31a6d70cc2d24c736ce6d6caeeaf
Aishani Mohapatra <mohapatraaishani20@gmail.com>
2023-07-10 23:23:40 +0530

    Find the element that appears once in sorted array

--- Hashing/Find the element that appears once in sorted array.cpp
+++ Hashing/Find the element that appears once in sorted array.cpp
@@ -14,3 +14,36 @@ class Solution
         
     }
 };
+
+
+//APPROACH-2 (MAP)
+class Solution
+{
+  public:
+    int findOnce(int arr[], int n)
+    {
+        //code here.
+        // int ans=0;
+        // for(int i=0;i<n;i++){
+        // ans=ans^arr[i];
+        
+        // }
+        // return ans;
+        
+        unordered_map<int,int>mp;
+        //iterate through array
+        for(int i=0;i<n;i++){
+            mp[arr[i]]++;
+        }
+            //iterate through map
+            for(auto x:mp){
+            //here x.first reprsents the key and x.second represents the corresponding value.
+            if(x.second==1){
+                return x.first;
+                break;
+            }
+        }
+        
+    
+    }
+};
